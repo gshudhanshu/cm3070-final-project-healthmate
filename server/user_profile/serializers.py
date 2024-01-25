dc843ec59d55262d2309d134bf0279a8fdec9105
Shudhanshu Gunjal <33764308+gshudhanshu@users.noreply.github.com>
2024-01-25 11:56:09 +0530

    language serializer update

--- server/user_profile/serializers.py
+++ server/user_profile/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import Doctor, Patient, Language, Speciality, Qualification
+from .models import Doctor, Patient, Language, LanguageProficiency, Speciality, Qualification
 
 User = get_user_model()
 
@@ -10,10 +10,14 @@ class UserProfileSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'first_name', 'last_name', 'email']
         ref_name = 'UserProfile'
         
-class LanguageSerializer(serializers.ModelSerializer):
+        
+class LanguageProficiencySerializer(serializers.ModelSerializer):
+    language_name = serializers.ReadOnlyField(source='language.name')
+    
     class Meta:
-        model = Language
-        fields = '__all__'
+        model = LanguageProficiency
+        fields = ['language_name', 'level']
+  
         
 class SpecialitySerializer(serializers.ModelSerializer):
     class Meta:
@@ -28,7 +32,8 @@ class QualificationSerializer(serializers.ModelSerializer):
 class DoctorSerializer(serializers.ModelSerializer):
     user = UserProfileSerializer(read_only=True)
     specialties = SpecialitySerializer(many=True, read_only=True)
-    languages = LanguageSerializer(many=True, read_only=True)
+    language_proficiencies = LanguageProficiencySerializer(source='languageproficiency_set', many=True, read_only=True)
+
     qualifications = QualificationSerializer(many=True, read_only=True)
     
     class Meta:
