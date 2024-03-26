09a4950c1ab22f0ee403e965aae0d6feb0505c7c
Shudhanshu Gunjal <33764308+gshudhanshu@users.noreply.github.com>
2024-03-26 22:03:44 +0530

    redis 1

--- server/server/settings.py
+++ server/server/settings.py
@@ -266,7 +266,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(os.environ.get("REDIS_URL", "127.0.0.1"), os.environ.get("REDIS_PORT", "6379"))],
+            "hosts": [(os.environ.get("REDIS_HOST", "127.0.0.1"), os.environ.get("REDIS_PORT", "6379"))],
         },
     },
 }
