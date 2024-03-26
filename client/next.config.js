65f1ad957400e30e45c55bf21ef80a8eb48fdeb2
Shudhanshu Gunjal <33764308+gshudhanshu@users.noreply.github.com>
2024-03-26 12:45:57 +0530

    env update

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // env: {
-  //   API_URL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/api",
-  //   NEXT_PUBLIC_BACKEND_URL:
-  //     process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000",
-  //   SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL || "ws://127.0.0.1:8000",
-  // },
+  env: {
+    NEXT_PUBLIC_API_URL:
+      process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/api",
+    NEXT_PUBLIC_BACKEND_URL:
+      process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000",
+    NEXT_PUBLIC_SOCKET_URL:
+      process.env.NEXT_PUBLIC_SOCKET_URL || "ws://127.0.0.1:8000",
+  },
   // placeholder images placehold.co
   images: {
     domains: ["127.0.0.1", "placehold.co"],
