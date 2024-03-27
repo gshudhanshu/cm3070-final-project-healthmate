908a076ff8a724db128195d7ea33d9f5cffca64a
Shudhanshu Gunjal <33764308+gshudhanshu@users.noreply.github.com>
2024-03-27 19:34:18 +0530

    update

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -8,7 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
